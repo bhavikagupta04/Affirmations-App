ab7d2d6558a974ab642054e155dd4da6e22ec577
Bhavika Gupta <142746783+bhavikagupta04@users.noreply.github.com>
2024-11-30 17:36:31 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,59 @@
+print("Welcome to your daily affirmations")
+name = input("What is your name? ")
+print(f"hello {name}! Nice to meet you.")
+print()
+
+
+if name == "Mark" or name == "mark":
+  day = input("What day of the week is it currently? ")
+  print(f"Ah, what a beautiful {day} it is!")
+  print()
+  fav_thing1 = input("What is one of your favorite things? ")
+  print("Got it.")
+  print()
+
+  fav_thing2 = input("What is another one of your favorite things? ")
+  print("Very cool!")
+  print()
+  if day.lower() == "monday":
+    print(f"{name}. {day}'s are a great day to be thankful for everything beautiful around you in the world. Take time to appreciate {fav_thing1} and {fav_thing2}.")
+  if day.lower() == "tuesday":
+    print(f"{name}. {day}'s are a great day to be thankful for everything beautiful around you in the world. Take time to appreciate {fav_thing1} and {fav_thing2}.")
+  if day.lower() == "wednesday":
+      print(f"{name}. {day}'s are a great day to be thankful for everything beautiful around you in the world. Take time to appreciate {fav_thing1} and {fav_thing2}.")
+  if day.lower() == "thursday":
+      print(f"{name}. {day}'s are a great day to be thankful for everything beautiful around you in the world. Take time to appreciate {fav_thing1} and {fav_thing2}.")
+  if day.lower() == "friday":
+      print(f"{name}. {day}'s are a great day to be thankful for everything beautiful around you in the world. Take time to appreciate {fav_thing1} and {fav_thing2}.")
+  if day.lower() == "saturday":
+      print(f"{name}. {day}'s are a great day to be thankful for everything beautiful around you in the world. Take time to appreciate {fav_thing1} and {fav_thing2}.")
+  if day.lower() == "sunday":
+      print(f"{name}. {day}'s are a great day to be thankful for everything beautiful around you in the world. Take time to appreciate {fav_thing1} and {fav_thing2}.")
+
+elif name == "Jason" or name == "jason":
+  day = input("What day of the week is it currently? ")
+  print(f"Ah, what a beautiful {day} it is!")
+  print()
+  fav_thing1 = input("What is one of your favorite things? ")
+  print("Got it.")
+  print()
+
+  fav_thing2 = input("What is another one of your favorite things? ")
+  print("Very cool!")
+  print()
+  if day.lower() == "monday":
+    print(f"{name}. {day}'s are a great day to be thankful for everything beautiful around you in the world. Take time to appreciate {fav_thing1} and {fav_thing2}.")
+  if day.lower() == "tuesday":
+    print(f"{name}. {day}'s are a great day to be thankful for everything beautiful around you in the world. Take time to appreciate {fav_thing1} and {fav_thing2}.")
+  if day.lower() == "wednesday":
+      print(f"{name}. {day}'s are a great day to be thankful for everything beautiful around you in the world. Take time to appreciate {fav_thing1} and {fav_thing2}.")
+  if day.lower() == "thursday":
+      print(f"{name}. {day}'s are a great day to be thankful for everything beautiful around you in the world. Take time to appreciate {fav_thing1} and {fav_thing2}.")
+  if day.lower() == "friday":
+      print(f"{name}. {day}'s are a great day to be thankful for everything beautiful around you in the world. Take time to appreciate {fav_thing1} and {fav_thing2}.")
+  if day.lower() == "saturday":
+      print(f"{name}. {day}'s are a great day to be thankful for everything beautiful around you in the world. Take time to appreciate {fav_thing1} and {fav_thing2}.")
+  if day.lower() == "sunday":
+      print(f"{name}. {day}'s are a great day to be thankful for everything beautiful around you in the world. Take time to appreciate {fav_thing1} and {fav_thing2}.")
+else:
+  print("That's not a valid day, silly!")
